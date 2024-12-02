8ffb4869510cabd4baf859fd8be1297103e32398
Griffin Cox <griffin@ezbot.ai>
2024-12-02 13:33:20 -0600

    implemented clerk in side panel, working

--- src/pages/panel/Panel.tsx
+++ src/pages/panel/Panel.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import '@pages/panel/Panel.css';
+import React from "react";
+import "@pages/panel/Panel.css";
+import { ClerkProvider, SignInButton } from "@clerk/chrome-extension";
+
+const PUBLISHABLE_KEY =
+  "pk_test_YmFsYW5jZWQtbW9sbHktODMuY2xlcmsuYWNjb3VudHMuZGV2JA";
+
+const EXTENSION_URL = chrome.runtime.getURL(".");
 
 export default function Panel(): JSX.Element {
   return (
-    <div className="container">
-      <h1>Dev Tools Panel</h1>
-    </div>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl={`${EXTENSION_URL}/popup.html`}
+      signInFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+      signUpFallbackRedirectUrl={`${EXTENSION_URL}/popup.html`}
+    >
+      <div className="container">
+        <h1>Dev Tools Panel</h1>
+        <SignInButton mode="modal" />
+      </div>
+    </ClerkProvider>
   );
 }
